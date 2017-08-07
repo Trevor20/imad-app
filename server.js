759bb0dd43d2916c2d365813ba866ba236008c51
Trevor20 <30793779+Trevor20@users.noreply.github.com>
2017-08-08 00:16:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,7 +15,14 @@ app.get('/ui/style.css', function (req, res) {
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+ 
 });
+
+app.get('/ui/newfile.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'newfile.html'));
+ 
+});
+
 app.get('/ui/trevor-1',function(req,res){
     res.send('This is awesome 1');
     
